ef97c60419d80a187158b785727a23e044695f9d
Nguyen Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2020-08-12 12:03:13 +0700

    Add files via upload

--- Source/FCShell.bat
+++ Source/FCShell.bat
@@ -16,10 +16,30 @@ if "%command%" == "GrabMeADrink[str]" goto GrabMeADrink
 if "%command%" == "Loop[fnc]" goto Loop
 if "%command%" == "Sleep[fnc]" goto Sleep
 if "%command%" == "Shutdown[fnc]" goto Shutdown
+if "%command%" == "Forget[fnc]" goto Forget
+if "%command%" == "GotoToilet[fnc]" goto GotoToilet
 if "%command%" == "Restart[fnc]" goto Restart
 if "%command%" == "EnterMatrix[fnc]" goto EnterMatrix
 if "%command%" == "EnterMatrixHacker[fnc]" goto EnterMatrixHacker
 if "%command%" == "ILoveYou[str]" goto ILoveYou
+for %%a in (%command%) do (
+	set printString=%command%
+	if %%a == Swear[fnc] (
+		echo !printString:~11!
+	)
+	if %%a == PukeDir[fnc] (
+		md !printString:~13!
+	)
+	if %%a == EatDir[fnc] (
+		cd !printString:~12!
+	)
+	if %%a == StealDiary[fnc] (
+		type !printString:~16!
+	)
+	if %%a == HackerMan[fnc] (
+		echo !printString:~15!
+	)
+)
 goto c2
 
 :ILoveYou
@@ -27,11 +47,11 @@ echo I love you!
 goto c2
 
 :Shutdown
-shutdown -s
+shutdown -s -t 0
 goto c2
 
 :Restart
-shutdown -r
+shutdown -r -t 0
 goto c2
 
 :HelloWorld
@@ -79,3 +99,11 @@ echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%ra
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
 goto matrix
+
+:Forget
+cls
+goto c2
+
+:GotoToilet
+pause
+goto c2
--- Source/FreakC.bat
+++ Source/FreakC.bat
@@ -1,33 +1,68 @@
 @echo off
+@setlocal enabledelayedexpansion
 if "%1" == "" fcshell
 set a=%1
-set /a rnd=%random% %% 99 + 0
-set /a rnd1=%random% %% 999 + 0
-set /a rnd2=%random% %% 9 + 0
+set b=0
 echo @echo off>%a%.bat
 echo :FreakCCompiled>>%a%.bat
 for /f "tokens=* delims= " %%x in (%a%.fclang) do (
-if %%x == HelloWorld[str] echo echo Hello, World!>>%a%.bat
-if %%x == ILoveYou[str] echo echo I love you!>>%a%.bat
-if %%x == GrabMeADrink[str] echo echo Grab me a drink!>>%a%.bat
-if %%x == RandomNumMinimum[int] echo echo %rnd%>>%a%.bat
-if %%x == RandomNumShort[int] echo echo %rnd1%>>%a%.bat
-if %%x == RandomNum[int] echo echo %random%>>%a%.bat
-if %%x == Loop[fnc] echo goto FreakCCompiled>>%a%.bat
-if %%x == Sleep[fnc] echo timeout %rnd2% /nobreak>>%a%.bat
-if %%x == ShutdownSystem[fnc] echo shutdown -s>>%a%.bat
-if %%x == RestartSystem[fnc] echo shutdown -r>>%a%.bat
-if %%x == EnterMatrix[fnc] (echo :matrix>>%a%.bat
-echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-echo goto matrix>>%a%.bat)
-if %%x == EnterMatrixHacker[fnc] (echo color 0a>>%a%.bat
-echo :matrix>>%a%.bat
-echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-echo goto matrix>>%a%.bat))
+	for %%a in (%%x) do (
+		set printString=%%x
+		if %%a == Swear[fnc] (
+			echo echo !printString:~11!>>%a%.bat
+		)
+		if %%a == PukeDir[fnc] (
+			echo md !printString:~13!>>%a%.bat
+		)
+		if %%a == EatDir[fnc] (
+			echo cd !printString:~12!>>%a%.bat
+		)
+		if %%a == StealDiary[fnc] (
+			echo type !printString:~16!>>%a%.bat
+		)
+		if %%a == HackerMan[fnc] (
+			echo !printString:~15!>>%a%.bat
+		)
+	)
+	if %%x == HelloWorld[str] echo echo Hello, World!>>%a%.bat
+	if %%x == ILoveYou[str] echo echo I love you!>>%a%.bat
+	if %%x == GrabMeADrink[str] echo echo Grab me a drink!>>%a%.bat
+	if %%x == RandomNumMinimum[int] (
+		set /a rnd=%random% %% 99 + 0
+		echo echo !rnd!>>%a%.bat 
+	)
+	if %%x == RandomNumShort[int] (
+		set /a rnd1=%random% %% 999 + 0
+		echo echo !rnd1!>>%a%.bat
+	)
+	if %%x == RandomNum[int] (
+		echo echo %random%>>%a%.bat
+	)
+	if %%x == Sleep[fnc] (
+		set /a rnd2=%random% %% 9 + 0
+		echo timeout !rnd2! /nobreak>>%a%.bat
+	)
+	if %%x == Loop[fnc] echo goto FreakCCompiled>>%a%.bat
+	if %%x == ShutdownSystem[fnc] echo shutdown -s -t 0>>%a%.bat
+	if %%x == RestartSystem[fnc] echo shutdown -r -t 0>>%a%.bat
+	if %%x == Forget[fnc] echo cls>>%a%.bat
+	if %%x == GotoToilet[fnc] echo pause>>%a%.bat
+	if %%x == EnterMatrix[fnc] (
+		echo :matrix>>%a%.bat
+		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
+		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
+		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
+		echo goto matrix>>%a%.bat
+	)
+	if %%x == EnterMatrixHacker[fnc] (
+		echo color 0a>>%a%.bat
+		echo :matrix>>%a%.bat
+		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
+		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
+		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
+		echo goto matrix>>%a%.bat
+	)
+)
 echo pause>>%a%.bat
 call %a%.bat
 @echo on

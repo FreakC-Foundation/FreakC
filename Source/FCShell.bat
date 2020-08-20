74c3785a47190c3ac3af35c5bd488b1353a46061
Nguyen Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2020-08-20 15:27:55 +0700

    Add files via upload

--- Source/FCShell.bat
+++ Source/FCShell.bat
@@ -9,20 +9,11 @@ echo.
 echo.
 :c2
 set /p command=~
-if "%command%" == "HelloWorld[str]" goto HelloWorld
-if "%command%" == "RandomNumMinimum[int]" goto RandomNumMinimum
-if "%command%" == "RandomNumShort[int]" goto RandomNumShort
-if "%command%" == "RandomNum[int]" goto RandomNum
-if "%command%" == "GrabMeADrink[str]" goto GrabMeADrink
 if "%command%" == "Loop[fnc]" goto Loop
-if "%command%" == "Sleep[fnc]" goto Sleep
 if "%command%" == "Shutdown[fnc]" goto Shutdown
 if "%command%" == "Forget[fnc]" goto Forget
 if "%command%" == "GotoToilet[fnc]" goto GotoToilet
 if "%command%" == "Restart[fnc]" goto Restart
-if "%command%" == "EnterMatrix[fnc]" goto EnterMatrix
-if "%command%" == "EnterMatrixHacker[fnc]" goto EnterMatrixHacker
-if "%command%" == "ILoveYou[str]" goto ILoveYou
 if "%command%" == "Die[fnc]" exit
 if "%command%" == "MyMemory[fnc]" dir
 if "%command%" == "SeeTime[fnc]" time /t
@@ -59,10 +50,6 @@ for %%a in (%command%) do (
 )
 goto c2
 
-:ILoveYou
-echo I love you!
-goto c2
-
 :Shutdown
 shutdown -s -t 0
 goto c2
@@ -71,51 +58,9 @@ goto c2
 shutdown -r -t 0
 goto c2
 
-:HelloWorld
-echo Hello, World!
-goto c2
-
-:RandomNumMinimum
-set /a rnd=%random% %% 99 + 0 
-echo %rnd%
-goto c2
-
-:RandomNumShort
-set /a rnd=%random% %% 999 + 0
-echo %rnd%
-goto c2
-
-:RandomNum
-echo %random%
-goto c2
-
-:GrabMeADrink
-echo Grab me a drink!
-goto c2
-
-:Sleep
-set /a rnd=%random% %% 9 + 0
-timeout %rnd% /nobreak
-goto c2
-
 :Loop
 echo Sorry but Loop[fnc] doesn't work on FCShell
 goto c2
-
-:EnterMatrix
-:matrix
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-goto matrix
-
-:EnterMatrixHacker
-color 0a
-:matrix
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-goto matrix
 
 :Forget
 cls
--- Source/FreakC.bat
+++ Source/FreakC.bat
@@ -9,74 +9,42 @@ echo :FreakCCompiled>>%a%.bat
 for /f "tokens=* delims= " %%x in (%a%.fclang) do (
 	for %%a in (%%x) do (
 		set printString=%%x
-		if %%a == Swear[fnc] echo echo !printString:~11!>>%a%.bat
-		if %%a == SwearLine[fnc] echo echo. !printString:~15!>>%a%.bat
-		if %%a == PukeDir[fnc] echo md !printString:~13!>>%a%.bat
-		if %%a == EatDir[fnc] echo cd !printString:~12!>>%a%.bat
-		if %%a == Trash[fnc] echo del !printString:~11!>>%a%.bat
-		if %%a == TrashDir[fnc] echo rmdir !printString:~14!>>%a%.bat
-		if %%a == EatDrive[fnc] echo !printString:~14!:>>%a%.bat
-		if %%a == StealDiary[fnc] echo type !printString:~16!>>%a%.bat
-		if %%a == ReadDiary[fnc] echo call !printString:~15!>>%a%.bat
-		if %%a == HackerMan[fnc] echo !printString:~15!>>%a%.bat
-		if %%a == WaitForBus[fnc] echo timeout /t !printString:~16! /nobreak>>%a%.bat
+		if %%a == Swear[fnc] echo !printString:Swear[fnc]=echo!>>%a%.bat
+		if %%a == SwearLine[fnc] echo !printString:SwearLine[fnc]=echo.!>>%a%.bat
+		if %%a == PukeDir[fnc] echo !printString:PukeDir[fnc]=md!>>%a%.bat
+		if %%a == EatDir[fnc] echo !printString:EatDir[fnc]=cd!>>%a%.bat
+		if %%a == Trash[fnc] echo !printString:Trash[fnc]=del!>>%a%.bat
+		if %%a == TrashDir[fnc] echo !printString:TrashDir[fnc]=rmdir!>>%a%.bat
+		if %%a == EatDrive[fnc] echo !printString:EatDrive[fnc] =!:>>%a%.bat
+		if %%a == StealDiary[fnc] echo !printString:StealDiary[fnc]=type!>>%a%.bat
+		if %%a == ReadDiary[fnc] echo !printString:ReadDiary[fnc]=call!>>%a%.bat
+		if %%a == HackerMan[fnc] echo !printString:HackerMan[fnc] =!>>%a%.bat
+		if %%a == WaitForBus[fnc] echo !printString:WaitForBus[fnc]=timeout /t! /nobreak>>%a%.bat
 		if %%a == OddOrEven[fnc] (
-			set /a num=!printString:~15! %% 2>>%a%.bat
-			if !num! == 0 set result=Is even>>%a%.bat
-			if not !num! == 0 set result=Is odd>>%a%.bat
-			echo echo !result!>>%a%.bat
+			set /a num=!printString:~14! %% 2
+			if !num! == 0 echo echo Is even>>%a%.bat
+			if not !num! == 0 echo echo Is odd>>%a%.bat
 		)
-		if %%a == PoopInt[fnc] echo set /a !printString:~13!>>%a%.bat
-		if %%a == PoopString[fnc] echo set !printString:~16!>>%a%.bat
-		if %%a == PoopInput[fnc] echo set /p !printString:~15!>>%a%.bat
-		if %%a == PoopFnc[fnc] echo :!printString:~13!>>%a%.bat
-		if %%a == EatFnc[fnc] echo goto !printString:~12!>>%a%.bat
-		if %%a == GrabFnc[fnc] echo call !printString:~13!>>%a%.bat
-		if %%a == ChangeColor[fnc] echo color !printString:~17!>>%a%.bat
-		if %%a == ChangeName[fnc] echo title !printString:~16!>>%a%.bat
-		if %%a == ChangeSize[fnc] echo mode !printString:~16!>>%a%.bat
-		if %%a == RenameFile[fnc] echo ren !printString:~16!>>%a%.bat
-		if %%a == MoveFile[fnc] echo move !printString:~14!>>%a%.bat
-		if %%a == CopyFile[fnc] echo copy !printString:~14!>>%a%.bat
-		if %%a == PlayFile[fnc] echo start !printString:~14!>>%a%.bat
+		if %%a == PoopInt[fnc] echo !printString:PoopInt[fnc]=set /a!>>%a%.bat
+		if %%a == PoopString[fnc] echo !printString:PoopString[fnc]=set!>>%a%.bat
+		if %%a == PoopInput[fnc] echo !printString:PoopInput[fnc]=set /p!>>%a%.bat
+		if %%a == PoopFnc[fnc] echo :!printString:PoopFnc[fnc] =!>>%a%.bat
+		if %%a == EatFnc[fnc] echo !printString:EatFnc[fnc]=goto!>>%a%.bat
+		if %%a == GrabFnc[fnc] echo !printString:GrabFnc[fnc]=call!>>%a%.bat
+		if %%a == ChangeColor[fnc] echo !printString:ChangeColor[fnc]=color!>>%a%.bat
+		if %%a == ChangeName[fnc] echo !printString:ChangeName[fnc]=title!>>%a%.bat
+		if %%a == ChangeSize[fnc] echo !printString:ChangeSize[fnc]=mode!>>%a%.bat
+		if %%a == RenameFile[fnc] echo !printString:RenameFile[fnc]=ren!>>%a%.bat
+		if %%a == MoveFile[fnc] echo !printString:MoveFile[fnc]=move!>>%a%.bat
+		if %%a == CopyFile[fnc] echo !printString:CopyFile[fnc]=copy!>>%a%.bat
+		if %%a == PlayFile[fnc] echo !printString:PlayFile[fnc]=start!>>%a%.bat
 	)
 	if %%x == Die[fnc] echo exit>>%a%.bat
-	if %%x == HelloWorld[str] echo echo Hello, World!>>%a%.bat
-	if %%x == ILoveYou[str] echo echo I love you!>>%a%.bat
-	if %%x == GrabMeADrink[str] echo echo Grab me a drink!>>%a%.bat
-	if %%x == RandomNumMinimum[int] (
-		set /a rnd=%random% %% 99 + 0
-		echo echo !rnd!>>%a%.bat 
-	)
-	if %%x == RandomNumShort[int] (
-		set /a rnd1=%random% %% 999 + 0
-		echo echo !rnd1!>>%a%.bat
-	)
-	if %%x == RandomNum[int] echo echo %random%>>%a%.bat
-	if %%x == Sleep[fnc] (
-		set /a rnd2=%random% %% 9 + 0
-		echo timeout !rnd2! /nobreak>>%a%.bat
-	)
 	if %%x == Loop[fnc] echo goto FreakCCompiled>>%a%.bat
 	if %%x == ShutdownSystem[fnc] echo shutdown -s -t 0>>%a%.bat
 	if %%x == RestartSystem[fnc] echo shutdown -r -t 0>>%a%.bat
 	if %%x == Forget[fnc] echo cls>>%a%.bat
 	if %%x == GotoToilet[fnc] echo pause>>%a%.bat
-	if %%x == EnterMatrix[fnc] (
-		echo :matrix>>%a%.bat
-		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-		echo goto matrix>>%a%.bat
-	)
-	if %%x == EnterMatrixHacker[fnc] (
-		echo color 0a>>%a%.bat
-		echo :matrix>>%a%.bat
-		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-		echo echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%>>%a%.bat
-		echo goto matrix>>%a%.bat
-	)
 	if %%x == OpenHouse[fnc] echo SETLOCAL>>%a%.bat
 	if %%x == CloseHouse[fnc] echo ENDLOCAL>>%a%.bat
 	if %%x == SeeTime[fnc] echo date /t>>%a%.bat

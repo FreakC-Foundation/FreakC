a31d7a40640a4381088673c565d10ee720f12f10
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2021-07-12 11:50:10 +0700

    Add files via upload

--- Source/FreakC.bat
+++ Source/FreakC.bat
@@ -476,5 +476,5 @@ if "%fcread%" == "true" type %a%.bat
 if not "%fccompile%" == "true" if not "%fcread%" == "true" call %a%.bat
 exit /b
 :fcversion
-echo FreakC DevKit Version 0.1.0
+echo FreakC DevKit Version 0.3.0
 exit /b

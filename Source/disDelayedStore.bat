c27c37b82101e760e4bb9b05a62495f2bc4c0eb8
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2021-10-02 19:29:30 +0700

    Change stuffs so that inheritance can work

--- Source/disDelayedStore.bat
+++ Source/disDelayedStore.bat
@@ -1,4 +1,6 @@
 setlocal disableDelayedExpansion
-set name=%~2
-type %~1>%name:.method=.bat%
+set name1=%~1
+set name2=%~2
+type %name1%>%name1:.method=.bat%
+type %name1%>%name2:.method=.bat%
 exit /b
